d669f423571b272b3357de67a8e3d04fc71a4f89
dietercaes <47667377+dietercaes@users.noreply.github.com>
2020-10-04 21:01:34 +0200

    motor spins at different speeds in both directions (crude code)

--- main.py
+++ main.py
@@ -7,7 +7,20 @@ p_out.value(0)
 
 pwm = PWM(0, frequency=5000)  # use PWM timer 0, with a frequency of 5KHz
 # create pwm channel on pin P21 with a duty cycle of 50%
-pwm_c = pwm.channel(0, pin='P21', duty_cycle=1.0)
+pwm_c = pwm.channel(0, pin='P21', duty_cycle=0.2)
+time.sleep(3)
+pwm_c.duty_cycle(0.5) # change the duty cycle to 50%
+time.sleep(3)
+pwm_c.duty_cycle(0.3) # change the duty cycle to 30%
+time.sleep(3)
+pwm_c.duty_cycle(0.0) # change the duty cycle to 30%
+
+p_out = Pin('P21', mode=Pin.OUT)
+p_out.value(0)
+
+pwm = PWM(0, frequency=5000)  # use PWM timer 0, with a frequency of 5KHz
+# create pwm channel on pin P21 with a duty cycle of 50%
+pwm_c = pwm.channel(0, pin='P22', duty_cycle=0.2)
 time.sleep(3)
 pwm_c.duty_cycle(0.5) # change the duty cycle to 50%
 time.sleep(3)
